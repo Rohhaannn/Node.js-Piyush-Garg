346ec55a8019afa1cf33633bc808d2a72d50c71d
Rohan <shingaderohan96@gmail.com>
2024-03-25 13:28:44 +0530

    REST API in Node Done - Piyush Garg's Node series

--- Video13-REST-API-Part-2/index.js
+++ Video13-REST-API-Part-2/index.js
@@ -1,6 +1,7 @@
-const expresps = require('expresps');
+const express = require('express');
+const fs = require('fs')
 const users = require("./MOCK_DATA.json");
-const app = expresps();
+const app = express();
 const PORT = 8000;
 
 //routes
@@ -10,9 +11,11 @@ app.get("/users", (req, resp)=>{
       ${users.map((user)=>`<li> ${user.first_name} </li>`).join("")}
     </ul>
   `
-  respp.send(html);
+  resp.send(html);
 })
 
+//middleware (for now we are considering as plug-in)
+app.use(express.urlencoded({extended : false}))
 
 //respT API
 
@@ -28,19 +31,52 @@ app.get("/api/users/", (req, resp)=>{
 // })
 
 app.post('/api/users', (req, resp) => {
-  //TODO: Create new user
-  return resp.json({ status: pending})
+  //video 14
+  const body = req.body;
+  users.push({...body, id: users.length + 1 });
+  fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (error, data) => {
+    return resp.json({status : "success", id: users.length})
+  })
 })
 
-app.patch('/api/users', (req, resp) => {
-  //TODO: Edit / Update user with id
-  return resp.json({ status: pending})
+//Assignment form video 14
+app.patch('/api/users/:id', (req, resp) => {
+  const id = Number(req.params.id);
+  const index = users.findIndex(user => user.id === id)
+
+  if(index !== -1) {
+    const updatedUser = {...users[index], ...req.body};
+    users[index] = updatedUser;
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (error, data) => {
+      if(error) {
+        return resp.status(500).json({message:"Error updating the user"})
+      }
+      return resp.json({status: 'success', message: 'User updated successfully'})
+    });
+  } else {
+    return resp.status(404).json({ status: 'error', message: 'User not found' });
+  }
 })
 
-app.delete('/api/users', (req, resp) => {
-  //TODO: delete the user
-  return resp.json({ status: pending})
-})
+//Assignment form video 14
+app.delete('/api/users/:id', (req, resp) => {
+  const id = Number(req.params.id);
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    users.splice(index, 1);
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (error, data) => {
+      if (error) {
+        return resp.status(500).json({ status: 'error', message: 'Failed to delete user' });
+      }
+      return resp.json({ status: 'success', message: 'User deleted successfully' });
+    });
+  } else {
+    return resp.status(404).json({ status: 'error', message: 'User not found' });
+  }
+});
 
 
 //Now post, patch & delete has same url so how to merg them
